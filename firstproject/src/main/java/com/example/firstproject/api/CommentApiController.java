88cc3772fe3d0fb061d3d7359fdece435c5cab98
leejonghwa <maymoon1235@gmail.com>
2026-01-18 21:10:26 +0900

    추가작업

--- firstproject/src/main/java/com/example/firstproject/api/CommentApiController.java
+++ firstproject/src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -2,13 +2,12 @@ package com.example.firstproject.api;
 
 import com.example.firstproject.dto.CommentDto;
 import com.example.firstproject.service.CommentService;
+import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.Comments;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,11 +21,18 @@ public class CommentApiController {
     public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
         // 서비스에 위임
         List<CommentDto> dtos = commentService.comments(articleId);
-
         // 결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(dtos);
     }
     // 2. 댓글 생성
+    @PostMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDto> create(@PathVariable Long articleId,
+                                             @RequestBody CommentDto dto) {
+        // 서비스에 위임
+        CommentDto createdDto = commentService.create(articleId, dto);
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(createdDto);
+    }
     // 3. 댓글 수정
     // 4. 댓글 삭제
 }
